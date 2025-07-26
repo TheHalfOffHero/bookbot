e5a5e00024aa8aec2d7f12b86b647b36725f8506
Matt Ruiz <matt.ruiz@mesh-systems.com>
2025-07-26 11:20:04 -0400

    adding two additional books and pulling filepath from sys.argv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words, get_num_characters, sorted_list_of_dict
+import sys
 
 def get_book_text(file_path):
   with open(file_path) as f:
@@ -6,9 +7,15 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-  file_contents = get_book_text("./books/frankenstein.txt")
+  if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+  else:
+    book_filepath = sys.argv[1]
+    
+  file_contents = get_book_text(book_filepath)
   print("============ BOOKBOT ============")
-  print("Analyzing book found at books/frankenstein.txt...")
+  print(f"Analyzing book found at {book_filepath}")
   print("----------- Word Count ----------")
   get_num_words(file_contents)
   print("--------- Character Count -------")
